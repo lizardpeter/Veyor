d5a0add3c3c7f102a534c355c1c2a30ee8b31ea8
Peter <53446060+lizardpeter@users.noreply.github.com>
2020-10-05 21:30:18 -0400

    Update Veyor.cpp

--- Veyor/Veyor.cpp
+++ Veyor/Veyor.cpp
@@ -5,17 +5,16 @@
 
 void hi()
 {
-	std::cout << "hi :)";
+	std::cout << "Function hi is running within the thread";
 }
 
 int main()
 {
-	std::cout << "My program starts" << std::endl;
 	startThread thread;
 	thread.run(thread.find, "Thread 1", hi);
 	startThread thread2;
 	thread2.run(thread.find, "Thread 2", hi);
-
+	std::cout << "Initialization is complete" << std::endl;
 
 	std::this_thread::sleep_for(std::chrono::seconds(10));
 	{
@@ -27,7 +26,7 @@ int main()
 	thread.cv.notify_all();
 	thread2.cv.notify_all();
 
-	std::cout << "Thread stops immediately" << std::endl;
+	std::cout << "Threads stop immediately" << std::endl;
 	thread.my_thread.join();
 	thread2.my_thread.join();
 }
